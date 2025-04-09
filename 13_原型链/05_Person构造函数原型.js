1dc8114890c53f6fac5b9c00f37546ac53420d1f
YanFang <838641570@qq.com>
2025-04-09 20:25:04 +0800

    a

--- 13_原型链/05_Person构造函数原型.js
+++ 13_原型链/05_Person构造函数原型.js
@@ -6,3 +6,5 @@ console.log(Person.prototype); // {}
 // console.log(Object.getOwnPropertyDescriptors(Person.prototype));
 console.log(Person.prototype.__proto__); //[Object: null prototype] {}
 console.log(Person.prototype.__proto__.__proto__); // null
+
+// 原型链最顶层的原型对象就是Object的原型对象
